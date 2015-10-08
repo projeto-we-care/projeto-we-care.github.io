035077ece52acaaf506bc22473ad5ce65a78ef09
thulioph <thulioph@gmail.com>
2015-10-08 04:17:18 -0300

    Adds api code.

---
+++ api/api/controllers/JoinUsController.js
@@ -0,0 +1,11 @@
+/**
+ * JoinUsController
+ *
+ * @description :: Server-side logic for managing joinuses
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/api/models/JoinUs.js
@@ -0,0 +1,30 @@
+/**
+* JoinUs.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    nome: {
+      type: "string",
+      required: true
+    },
+    email: {
+      type: "string",
+      required: true
+    },
+    telefone: {
+      type: "string",
+      required: true,
+      minLength: 8
+    },
+    mensagem: {
+      type: "string",
+      required: true
+    }
+  }
+};
+
